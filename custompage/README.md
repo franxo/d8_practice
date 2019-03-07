3d01e79a426ad9747ad97b87ed4d1a6ccd8bf1fc
franciscog <franciscog@unops.org>
2019-03-07 16:39:12 +0100

    add custom page with params example

--- custompage/README.md
+++ custompage/README.md
@@ -8,3 +8,4 @@ This module creates custom pages.
 - One page for module help (admin/help/custompage).
 - One single page (custompage/simple).
 - One custom page with custom template (custompage/themeable).
+- One custom page with params (custompage/params/1/12).
--- custompage/src/Controller/CustomPageController.php
+++ custompage/src/Controller/CustomPageController.php
@@ -3,6 +3,7 @@
 namespace Drupal\custompage\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Controller routines for custompage routes.
@@ -30,6 +31,41 @@ class CustomPageController extends ControllerBase {
         'hola asdfas asdfas222',
       ],
     ];
+  }
+
+  /**
+   * Constructs a page with arguments.
+   *
+   * @param string $first
+   *   A string to use, should be a number.
+   * @param string $second
+   *   Another string to use, should be a number.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+   *   If the parameters are invalid.
+   *
+   * @return array
+   *   Renderable array.
+   */
+  public function params($first, $second) {
+    // Make sure you don't trust the URL to be safe! Always check for exploits.
+    if (!is_numeric($first) || !is_numeric($second)) {
+      // We will just show a standard "access denied" page in this case.
+      throw new AccessDeniedHttpException();
+    }
+
+    $list[] = $this->t("First number was @number.", ['@number' => $first]);
+    $list[] = $this->t("Second number was @number.", ['@number' => $second]);
+    $list[] = $this->t('The total was @number.', ['@number' => $first + $second]);
+
+    $render_content['page_example_arguments'] = [
+      // The theme function to apply to the #items.
+      '#theme' => 'item_list',
+      // The list itself.
+      '#items' => $list,
+      '#title' => $this->t('Argument Information'),
+    ];
+    return $render_content;
   }
 
 }
